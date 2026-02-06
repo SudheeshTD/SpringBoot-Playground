2dbb6e0e6f32afbdf62a27b4708c7d99a2f2cf7e
Sudheesh T D <sudheeshtd01@gmail.com>
2026-02-06 00:28:30 -0600

    Actuators and security basics

--- 01-Spring-boot-Overview/sampleProj/src/main/java/com/sudheesh/sampleProj/rest/FunRestController.java
+++ 01-Spring-boot-Overview/sampleProj/src/main/java/com/sudheesh/sampleProj/rest/FunRestController.java
@@ -1,10 +1,24 @@
 package com.sudheesh.sampleProj.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+
+    //inject Properties: Coach Name and team Name
+
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
+    @GetMapping("/teaminfo")
+    public String getTeamInfo(){
+        return "Coach: "+ coachName+"\nTeam: "+teamName; 
+    }
 
     @GetMapping("/")
     public String sayHello() {
---
+++ ReadMe.md
@@ -0,0 +1,30 @@
+# Important Java Springboot Commands
+
+- Compile a Java program:
+
+  ```
+  javac FileName.java
+  ```
+
+- Package a Maven project(with mvnw wrapper):
+
+  ```
+  mvnw package
+  ```
+
+- to force Maven to delete the old files and start fresh. Run this command:
+
+  ```
+  mvnw clean package
+  ```
+
+- Run a Spring Boot application:
+
+  ```
+  java -jar target\sampleProj-0.0.1-SNAPSHOT.jar
+  ```
+
+- Run a Spring Boot application using Maven:
+  ```
+    mvnw spring-boot:run
+  ```
