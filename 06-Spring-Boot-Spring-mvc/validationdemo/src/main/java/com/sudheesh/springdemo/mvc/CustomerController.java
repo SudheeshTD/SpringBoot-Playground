b48655cf6ff25bdb7f40e6c3b1eebfd7019ee6d4
Sudheesh T D <sudheeshtd01@gmail.com>
2026-02-15 19:45:29 -0600

    Spring MVC form Validation- InitBinder

--- 06-Spring-Boot-Spring-mvc/validationdemo/src/main/java/com/sudheesh/springdemo/mvc/CustomerController.java
+++ 06-Spring-Boot-Spring-mvc/validationdemo/src/main/java/com/sudheesh/springdemo/mvc/CustomerController.java
@@ -1,15 +1,24 @@
 package com.sudheesh.springdemo.mvc;
 
 import jakarta.validation.Valid;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CustomerController {
+
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder){
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+        dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+    }
 
     @GetMapping("/")
     public String showForm(Model themodel){
@@ -20,8 +29,10 @@ public class CustomerController {
 
     @PostMapping("/process-form")
     public String processForm(
-            @Valid @ModelAttribute("customer") Customer thecustomer,
+            @Valid @ModelAttribute("customer") Customer theCustomer,
             BindingResult theBindingResult){
+
+        System.out.println("Last Name: !" + theCustomer.getLastName() + "!");
         if (theBindingResult.hasErrors()) {
             return "customer-form";
 
